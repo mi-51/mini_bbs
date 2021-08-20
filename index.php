196d9f50e5eec9916b19e9019610240ddab6d901
mizuki <75738412+mizuki51@users.noreply.github.com>
2021-08-21 00:29:31 +0900

    third commit

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
+<?php
+session_start();
+require('dbconnect.php');
+ 
+if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()){
+  $_SESSION['time'] = time();
+ 
+  $members = $db->prepare('SELECT * FROM members WHERE id=?');
+  $members->execute(array($_SESSION['id']));
+  $member = $members->fetch();
+} else {
+  header('Location: login.php');
+  exit();
+}
+ 
+if(!empty($_POST)) {
+  if ($_POST['message'] !== '') {
+    $message = $db->prepare('INSERT INTO posts SET member_id=?, message=?, created=NOW()');
+    $message->execute(array(
+      $member['id'],
+      $_POST['message']
+    ));
+  }
+} 
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -18,7 +44,7 @@
   	<div style="text-align: right"><a href="logout.php">ログアウト</a></div>
     <form action="" method="post">
       <dl>
-        <dt>○○さん、メッセージをどうぞ</dt>
+        <dt><?php print(htmlspecialchars($members['name'], ENT_QUOTES)); ?>さん、メッセージをどうぞ</dt>
         <dd>
           <textarea name="message" cols="50" rows="5"></textarea>
           <input type="hidden" name="reply_post_id" value="" />
